ee27333d61a3a5dac97352620edfed004b78d2e7
ligf <ligf@careland.com.cn>
2017-08-21 11:25:55 +0800

    init the image loader as singleton

--- lightimageloaderlibrary/src/main/java/com/ligf/lightimageloader/ImageLoader.java
+++ lightimageloaderlibrary/src/main/java/com/ligf/lightimageloader/ImageLoader.java
@@ -4,4 +4,21 @@ package com.ligf.lightimageloader;
  * Created by ligf on 2017/8/21.
  */
 public class ImageLoader {
+
+    private static ImageLoader mInstance = null;
+
+    private ImageLoader(){
+
+    }
+
+    public static ImageLoader getInstance(){
+        if (mInstance == null){
+            synchronized (ImageLoader.class){
+                if (mInstance == null){
+                    mInstance = new ImageLoader();
+                }
+            }
+        }
+        return  mInstance;
+    }
 }
